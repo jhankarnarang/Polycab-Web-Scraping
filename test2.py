a857f8fa5b86076091c7fb73c9d56fc1d777ba8d
Jhankar Narang <85575169+jhankarnarang@users.noreply.github.com>
2023-05-24 18:30:37 +0530

    Add files via upload

--- test2.py
+++ test2.py
@@ -3,8 +3,12 @@ import requests
 import pandas as pd
 import csv
 
+csv_file = open('scrape.csv','w')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['Prod_img','Prod_name','Prod_feature','Prod_specification'])
 
-source = requests.get('https://swadeshplywoods.com/swadesh-flush-doors/')
+url = 'https://swadeshplywoods.com/swadesh-flush-doors/'
+source = requests.get(url)
 soup = BeautifulSoup(source.content, 'lxml')
 
 img = soup.find('div',class_='wpb_column vc_column_container vc_col-sm-4 mpc-column')
@@ -16,7 +20,10 @@ prod_name = p_info.h2.text
 print(prod_name)
 p_feature = p_info.ul.text.replace('\n\n\n\n','')
 print(p_feature)
-p_table = p_info.table.text.replace('\n\n','')
-print(p_table)
+dfs = pd.read_html(url)                                                                                                         
+df = dfs[0]
+print(df)
+df.to_csv('table.csv')
+csv_writer.writerow([prod_img,prod_name,p_feature])
 
  
